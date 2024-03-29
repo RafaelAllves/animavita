9d6109a1093e4ce67d398c9e70577f6cdd563a73
Rafael Alves <rafaelsralves91@gmail.com>
2024-03-29 19:42:27 -0300

    Use @nestjs/axios to make requests

--- apps/backend/src/frameworks/opencage.service.ts
+++ apps/backend/src/frameworks/opencage.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import * as https from 'https';
+import { HttpService } from '@nestjs/axios';
 import { GeolocationService } from 'src/geolocation/geolocation.service';
 
 @Injectable()
 export class OpenCageService extends GeolocationService {
+  constructor(private readonly httpService: HttpService) {
+    super();
+  }
+
   async reverseGeocoding(latitude: string, longitude: string): Promise<any> {
     if (!latitude || !longitude) {
       throw new Error('Latitude and longitude must be provided.');
@@ -11,28 +15,19 @@ export class OpenCageService extends GeolocationService {
 
     const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${process.env.OPENCAGE_KEY}`;
 
-    return new Promise((resolve, reject) => {
-      https.get(url, (response) => {
-        let data = '';
-
-        response.on('data', (chunk) => {
-          data += chunk;
-        });
-
-        response.on('end', () => {
-          const parsedData = JSON.parse(data);
-          if (parsedData && parsedData.results) {
-            resolve(parsedData.results.map(address => ({
-              region: address.components.city || address.components.municipality,
-              subregion: address.components.state ||address.components.county,
-            })));
-          } else {
-            reject(new Error('Failed to parse response data.'));
-          }
-        });
-      }).on('error', (error) => {
-        reject(error);
-      });
-    });
+    try {
+      const response = await this.httpService.get(url).toPromise();
+      const parsedData = response.data;
+      if (parsedData && parsedData.results) {
+        return parsedData.results.map(address => ({
+          region: address.components.city || address.components.municipality,
+          subregion: address.components.state || address.components.county,
+        }));
+      } else {
+        throw new Error('Failed to parse response data.');
+      }
+    } catch (error) {
+      throw new Error(`Error fetching data from OpenCage API: ${error.message}`);
+    }
   }
 }
